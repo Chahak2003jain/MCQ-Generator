a7b573a07f447d7340d489aeb96bfac3aaf3e56c
Chahak <chahak2003jain@gmail.com>
2024-02-28 20:48:55 +0530

    Structure updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 #for installing local package in virtual environment
+from setuptools import find_packages, setup
+
+setup (
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Chahak Jain',
+    author_email='chahakjain.0510@gmail.com',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
